d4c7381e42f3c80abf76004a4b5c1753a0584419
Jean Leconte II <jeanemileleconteii@gmail.com>
2020-11-07 18:56:20 -0800

    refactor docs

--- README.md
+++ README.md
@@ -4,16 +4,8 @@ Run on localhost 3000 via nodemon which will automatically restart the server fo
 
 Everything in GraphQl runs through a single endpoint, commonly called 'graphql'
 
-The graphql language is a typed language
+The graphql language is a typed query language ... you exectue a query to fetch data
 
-// query is to fetch data
-// mutation to change data -> create, update or delete
-// ! means can't be null ... has to return a list of strings or an empty list
-    // first ! means has be Strings
-// rootValue holds the resolvers, has to match the schema by name
-
-express-graphql  //exports a middleware function that takes incoming requests and funnels them to graphql query parser
-
-graphql         // generates graphql schema from the passed in strings
-
-// args is a built-in object keyword, will be null in the resolver if no parameters are passed in its matching query or mutator
+express-graphQl package connects schemas and resolvers
+	It also ships with `graphiql: true` so you can visit http://localhost:3000/graphql and play with the api
+  ctrl + space for autocompletion 
--- server.js
+++ server.js
@@ -1,11 +1,29 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { graphqlHTTP } = require('express-graphql'); // graphqlHttp is not a function w/o {} ... not a default vs. named import thing
-const { buildSchema } = require('graphql');
+const { buildSchema } = require('graphql'); // generates graphql schema from the passed in strings ... using backtickets / templates ... for a multiline string
 
 const app = express();
 
 app.use(bodyParser.json());
+
+// query & mutation are two keywords that buildSchema is looking for ... based on the graphql specification
+/*
+  schema {
+    query: RootQuery
+    mutation: RootMutation
+  }
+*/
+// RootQuery & RootMutation are just names of types that you defined ... since graphql is a typed query language
+
+// [String] means a list of strings
+  // first ! means has be Strings
+  // second ! means can't be null ... has to return a list of strings or an empty list
+// queries can also have arguments -> you could have events(booked: Boolean): [String!]!
+
+
+// 'rootValue' is the resolver.
+// // args is a built-in object keyword, will be null in the resolver if no parameters are passed in its matching query or mutator
 
 app.use('/graphql', graphqlHTTP({
   schema: buildSchema(`
